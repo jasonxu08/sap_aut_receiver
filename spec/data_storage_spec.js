a1666b46b08aad5608a6cccdd5f7aa5f807c5e09
Jason <chengzhe.xu@microfocus.com>
2020-10-27 17:53:02 +0800

    Change data format.

--- spec/data_storage_spec.js
+++ spec/data_storage_spec.js
@@ -3,75 +3,45 @@ var data_storage = require("../src/data_storage");
 
 describe("Test data storage", function () {
     describe("Test aut data", function () {
-        beforeEach(function () {
+        it("should successfully run addAut", function () {
             data_storage.autData["hash"] = {
                 "hash": "hash",
-                "properties": [
-                    { "name": "name1", "value": "value1" },
-                    { "name": "name2", "value": "value2" }]
+                "properties": {
+                    "name1": "value1",
+                    "name2": "value2"
+                }
             };
-        });
-        it("should successfully run findProperty", function () {
-            var propertyToFind = { "name": "name2", "value": "new value" };
-            var property = data_storage.autData.findProperty(data_storage.autData["hash"].properties, propertyToFind);
-            expect(JSON.stringify(property)).toEqual(JSON.stringify({ "name": "name2", "value": "value2" }));
-
-        });
-        it("should overwrite existing value when run addProperty", function () {
-            var propertyToAdd = { "name": "name2", "value": "new value" };
-            data_storage.autData.addProperty("hash", propertyToAdd);
-            expect(JSON.stringify(data_storage.autData["hash"].properties)).toEqual(JSON.stringify([
-                { "name": "name1", "value": "value1" },
-                { "name": "name2", "value": "new value" }]));
-        });
-        it("should add a new property when run addProperty", function () {
-            var propertyToAdd = { "name": "name3", "value": "new value" };
-            data_storage.autData.addProperty("hash", propertyToAdd);
-            expect(JSON.stringify(data_storage.autData["hash"].properties)).toEqual(JSON.stringify([
-                { "name": "name1", "value": "value1" },
-                { "name": "name2", "value": "value2" },
-                { "name": "name3", "value": "new value" }]));
-        });
-        it("should successfully run mergeProperties", function () {
-            var autToMerge = {
-                "properties": [
-                    { "name": "name1", "value": "new value" },
-                    { "name": "name3", "value": "new value" }]
-            };
-            data_storage.autData.mergeProperties("hash", autToMerge);
-            expect(JSON.stringify(data_storage.autData["hash"].properties)).toEqual(JSON.stringify([
-                { "name": "name1", "value": "new value" },
-                { "name": "name2", "value": "value2" },
-                { "name": "name3", "value": "new value" }]));
-        });
-        it("should successfully run addAut", function () {
             var aut1 = {
-                    "hash": "hash",
-                    "properties": [
-                        { "name": "name1", "value": "new value" },
-                        { "name": "name3", "value": "new value" }]
+                "hash": "hash",
+                "properties": {
+                    "name1": "new value",
+                    "name3": "new value"
+                }
             };
             var aut2 = {
-                    "hash": "new hash",
-                    "properties": [
-                        { "name": "name1", "value": "value1" },
-                        { "name": "name2", "value": "value2" }]
+                "hash": "new hash",
+                "properties": {
+                    "name1": "value1",
+                    "name3": "value3"
+                }
             };
             data_storage.autData.addAut(aut1);
             data_storage.autData.addAut(aut2);
             expect(JSON.stringify(data_storage.autData)).toEqual(JSON.stringify({
                 "hash": {
                     "hash": "hash",
-                    "properties": [
-                        { "name": "name1", "value": "new value" },
-                        { "name": "name2", "value": "value2" },
-                        { "name": "name3", "value": "new value" }]
+                    "properties": {
+                        "name1": "new value",
+                        "name2": "value2",
+                        "name3": "new value"
+                    }
                 },
                 "new hash": {
                     "hash": "new hash",
-                    "properties": [
-                        { "name": "name1", "value": "value1" },
-                        { "name": "name2", "value": "value2" }]
+                    "properties": {
+                        "name1": "value1",
+                        "name3": "value3"
+                    }
                 }
             }));
 
--- src/data_storage.js
+++ src/data_storage.js
@@ -4,44 +4,47 @@ const filename = "test.json";
 
 var dataStorage = {
     autData: {
+        mergeAuts: function(auts) {
+            if (typeof auts != "object") {
+                console.log("Input auts are invalid!");
+                return;
+            }
+
+            var keys = Object.keys(auts);
+            keys.forEach(function(key) {
+                this.addAut(auts[key]);
+            }.bind(this));
+        },
         addAut: function (autToAdd) {
             if (this[autToAdd.hash] != null) {
-                this.mergeProperties(autToAdd.hash, autToAdd);
+                this.mergeProperties(autToAdd);
             } else {
                 this[autToAdd.hash] = autToAdd;
             }
         },
-        mergeProperties: function (hashKey, autToAdd) {
-            autToAdd.properties.forEach(function(property) {
-                this.addProperty(hashKey, property);
+        mergeProperties: function (autToAdd) {
+            if (typeof autToAdd.properties != "object")
+                return;
+
+            var keys = Object.keys(autToAdd.properties);
+            keys.forEach(function(key) {
+                this[autToAdd.hash].properties[key] = autToAdd.properties[key];
             }.bind(this));
-        },
-        addProperty: function (hashKey, propertyToAdd) {
-            var property = this.findProperty(this[hashKey].properties, propertyToAdd);
-            if (property != null) {
-                property.value = propertyToAdd.value;
-            } else {
-                this[hashKey].properties.push(propertyToAdd);
-            }
-        }, 
-        findProperty: function(properties, propertyToFind) {
-            return properties.find(function (property) {
-                return property.name == propertyToFind.name;
-            });
         }
     },
     initData: function () {
         this.read(function(rawData) {
             if (rawData == null)
-                rawData = "";
+                return;
             try {
-                rawData = JSON.parse("{\"data\": [" + rawData + "]}");
+                rawData = JSON.parse(rawData);
             } catch (err) {
-    
+                console.log("Error in parsing local file!");
+                return;
             }
-            rawData.data.forEach(function(data) {
-                this.autData.addAut(data);
-            }.bind(this));
+
+            this.autData.mergeAuts(rawData);
+
         }.bind(this));
         
     },
@@ -62,13 +65,10 @@ var dataStorage = {
     {
         "type": "",
         "hash": "",
-        "properties": [{"name": "","value": ""},
-                        {"name": "","value": ""}],
+        "properties": {"name1": "value1",
+                        "name2": "value2"},
         "url": "",
-        "browser": {
-        "type": "",
-        "version": ""
-        }
+        "browser": ""
     } */
     messageValidation: function (message) {
         if (typeof message != "object" || typeof message.type != "string" || typeof message.hash != "string") {
@@ -76,22 +76,10 @@ var dataStorage = {
         }
 
         // Tolerate properties to be null
-        if (message.properties != null && !this.propertiesValidation(message.properties))
+        if (message.properties != null && typeof message.properties != "object")
             return false;
 
         if (typeof message.url != "string" || typeof message.browser != "string")
-            return false;
-
-        return true;
-    },
-    propertiesValidation: function (properties) {
-        if (!Array.isArray(properties))
-            return false;
-
-        var invalidProperty = properties.some(function(property) {
-            return typeof property.name != "string" || typeof property.value != "string";
-        });
-        if (invalidProperty)
             return false;
 
         return true;
